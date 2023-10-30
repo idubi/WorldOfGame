2d93e8ee7dfc0a3b585eea0b2e39e64d21c7f8ae
idubi <ido.bistry@gmail.com>
2023-10-30 20:10:59 +0200

    upps : dokerfile changes

--- startup/start.sh
+++ startup/start.sh
@@ -7,3 +7,5 @@ echo "logfile = ${logfile}"
 pip install -r ./tests/requirements.txt >> ${logfile}
 pip list >> ${logfile}
 
+docker build -t scores_server -f ./docker/Dockerfile  .
+docker run -d --name score_srv -p 30000:30000 scores_server
