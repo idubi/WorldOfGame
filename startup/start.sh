e3cb31b82c23a32f302f713454cf963bc0e64d50
idubi <ido.bistry@gmail.com>
2023-10-30 21:12:47 +0200

    dockerfile ready for deployment

--- startup/start.sh
+++ startup/start.sh
@@ -7,5 +7,5 @@ echo "logfile = ${logfile}"
 pip install -r ./tests/requirements.txt >> ${logfile}
 pip list >> ${logfile}
 
-docker build -t scores_server -f ./docker/Dockerfile  .
-docker run -d --name score_srv -p 30000:30000 scores_server
+docker build -t scores-server -f ./docker/Dockerfile-python-alpin .
+docker run -d --name scores-srv -p 8777:30000 scores-server
