91d04b9ee9e6e9f3377f92fb1e85d29f86ce452c
idubi <ido.bistry@gmail.com>
2023-11-28 03:53:09 +0200

    selenium webdriver update and add selenium to    requirements

--- tests/E2E_Score_Unites.py
+++ tests/E2E_Score_Unites.py
@@ -3,11 +3,11 @@ import unittest
 from e2e import is_service_available,validate_user_score_with_limits
 class E2E_Score_Unitest(unittest.TestCase):
     def test_score_service(self):
-        if not is_service_available("http://localhost:30000") :
+        if not is_service_available("http://localhost:8777") :
             self.fail("Selenium action failed: Element not found")
             
     def test_user_score_with_limits(self):
-        if not validate_user_score_with_limits("http://localhost:30000","idubi",1,1000) :
+        if not validate_user_score_with_limits("http://localhost:8777","idubi",1,1000) :
             self.fail("Selenium action failed: Element not found")
             
 
