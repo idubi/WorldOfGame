9a31a2f6b25a53dacfec4d603e51f686fda0a821
idubi <ido.bistry@gmail.com>
2023-12-10 22:17:43 +0200

    add delete inages condition to prevent fail on script if no image exists

--- jenkins/docker-commands/remove-wow-image.ps1
+++ jenkins/docker-commands/remove-wow-image.ps1
@@ -2,5 +2,10 @@
 $containerExists = docker ps -a --format "{{.Image}}" | Where-Object { $_ -like "*world-of-games*" }
 if ($containerExists) {
     docker rm -f wow-app
-	docker rmi -f $(docker images 'idubi/world-of-games' -q)
+	docker rmi -f $(docker images 'world-of-games' -q)
+}
+
+$repoExists = docker images 'idubi/world-of-games' 
+if ($repoExists) {
+    docker rmi -f $(docker images 'idubi/world-of-games' -q)    <# Action to perform if the condition is true #>
 }
