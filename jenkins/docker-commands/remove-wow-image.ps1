b951f788189aa0f24c636bc79e58244b2f49239b
idubi <ido.bistry@gmail.com>
2023-12-10 21:52:24 +0200

    updated powershell script to remove all of the  docker repos from local

--- jenkins/docker-commands/remove-wow-image.ps1
+++ jenkins/docker-commands/remove-wow-image.ps1
@@ -1,7 +1,6 @@
 # Check if the container exists
 $containerExists = docker ps -a --format "{{.Image}}" | Where-Object { $_ -like "*world-of-games*" }
-
 if ($containerExists) {
     docker rm -f wow-app
-	docker images | Where-Object { $_.Repository -like "*world-of-games*" } | ForEach-Object { docker rmi $_.ID }
+	docker rmi -f $(docker images 'idubi/world-of-games' -q)
 }
